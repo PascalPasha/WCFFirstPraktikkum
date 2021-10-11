5f421e148113c3e37685fe73e92a653a36d50ea8
PascalPasha <testiing1212@gmail.com>
2021-10-11 19:27:34 +0700

    Add project files.

---
+++ FirstWCFCODE1.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFFirst
+{
+
+    public class FirstWCFCODE1 : IFirstWCFCODE1
+    {
+        public string Message()
+        {
+            return "Hello, This is my first web service";
+        }
+    }
+}
